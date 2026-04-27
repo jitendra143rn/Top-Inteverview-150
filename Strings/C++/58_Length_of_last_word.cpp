017510e7ff98fe848d24a5c94c988dff95e3573b
Jitendra Kumar <99542823+jitendrakumardevp@users.noreply.github.com>
2026-04-27 10:15:04 +0530

    Implement lengthOfLastWord function in Solution class

---
+++ Strings/C++/58_Length_of_last_word.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+
+        int count = 0;
+        if(s.size() == 1)
+          return 1;
+
+        for (int i = s.size()-1; i >= 0; i--) {
+            if(s[i] !=' ')
+                count++;
+
+            if (s[i] == ' ' && count > 0)
+                break;
+        }
+        return count;
+    }
+};
