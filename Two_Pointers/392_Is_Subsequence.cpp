65bbf589ce9b58b2d0924a49faab4f0e8d701c0b
Jitendra Kumar <99542823+jitendrakumardevp@users.noreply.github.com>
2026-05-07 23:36:58 +0530

    Implement isSubsequence function in C++

---
+++ Two_Pointers/392_Is_Subsequence.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+
+        int i = 0;
+        int j = 0;
+        int slen = s.size();
+        int tlen = t.size();
+
+        while (i < slen && j < tlen) {
+            if (s[i] == t[j]) {
+                i++;
+            }
+
+            j++;
+        }
+        if (i == slen) {
+            return true;
+        }
+        
+        return false;
+    }
+};
